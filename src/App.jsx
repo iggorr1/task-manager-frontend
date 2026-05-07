8c271a6f5b799d775afd0bb989c7e6e81fea57bc
iggorr1 <igorkov05@gmail.com>
2026-05-07 19:08:56 +0300

    Move API URL to environment variable

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 import "./App.css";
 
-const API_URL = "https://api.wwwho.lol";
+const API_URL = import.meta.env.VITE_API_URL || "https://api.wwwho.lol";
 
 const STATUSES = [
   { label: "All tasks", value: "" },
