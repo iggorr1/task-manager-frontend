be2761b0ab1a3c5ab8f1f5a8ed7fb9e3b13a82fc
iggorr1 <igorkov05@gmail.com>
2026-05-09 11:18:17 +0300

    Move task pinning to icon button

--- src/App.jsx
+++ src/App.jsx
@@ -22,6 +22,10 @@ function PinIcon({ filled = false }) {
         d="M16 3l5 5-3 1-4 4v5l-2 2-3-6-6-3 2-2h5l4-4 1-3z"
         fill={filled ? "currentColor" : "none"}
       />
+      <path
+        d="M9.0 15.0L5.2 21"
+        fill="none"
+      />
     </svg>
   );
 }
