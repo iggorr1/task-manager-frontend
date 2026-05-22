0ed7ffaf1841fe5435de26b476592bad1b1083b9
iggorr1 <igorkov05@gmail.com>
2026-05-22 12:57:02 +0300

    Add Turnstile frontend config

--- src/api/taskFlowApi.js
+++ src/api/taskFlowApi.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 const API_URL = import.meta.env.VITE_API_URL || "https://api.wwwho.lol";
+export const TURNSTILE_SITE_KEY = import.meta.env.VITE_TURNSTILE_SITE_KEY || "";
 
 function authConfig(jwt) {
   return {
